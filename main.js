8b851d3c6175d0cf9459afe7f4905ff6a177b127
Ram Kumar Karuppusamy <ramkumark1@outlook.com>
2023-01-10 13:40:00 +0530

    Refactor using javascript classes
    1.Use classes instead of objects
    2.Add styling to the page

--- main.js
+++ main.js
@@ -1,129 +1,86 @@
-// Book Class
-class Book {
-  constructor(name, author) {
-    this.name = name;
-    this.author = author;
+
+class book {
+  constructor(bktitle, bkauthor) {
+    this.title = bktitle;
+    this.author = bkauthor;
   }
 }
 
-// UI Class or we can add later in Book 
-class UI {
-  // UI : Display Books to the DOM
-  static displayBooks() {
-    const books = Store.getBooks();
-
-    books.forEach(book => {
-      UI.addBookToList(book);
-    });
+class bookCollection {
+  constructor() {
+    this.books = this.#readLocalStorage();
   }
 
-  // UI : Add Books to the Table
-  static addBookToList(book) {
-    const list = document.querySelector('#display-book');
-    const tr = document.createElement('tr');
+  getBooks() {
+    return this.books;
+  }
 
-    tr.innerHTML = `
-        <td>${book.name}</td>
-        <td>${book.author}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">Remove</a></td>
+  addBook(book) {
+    this.books.push(book);
+    this.#writeLocalStorage();
+  }
+
+  removeBook(book) {
+    this.books.splice(this.books.indexOf(book), 1);
+    this.#writeLocalStorage();
+  }
+
+  #writeLocalStorage(){
+    localStorage.setItem('book-list', JSON.stringify(this.books));
+  }
+
+  #readLocalStorage(){
+    return JSON.parse(localStorage.getItem('book-list'));
+  }
+}
+
+// load booklist in main page 'display-book' section
+function loadBooksList() {
+  const displaySection = document.querySelector('.display-book');
+  let books = new bookCollection();
+  while (displaySection.firstChild) {
+    displaySection.removeChild(displaySection.firstChild);
+  }
+  let i=1;
+  books.getBooks().forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-card');
+    if(i%2===0){bookDiv.classList.add('book-card-grey');}
+    i+=1;
+    bookDiv.innerHTML = `<div class='text-content'>
+      <h4>"${book.title}"</h2>
+      <h4>by ${book.author}</h3>
+      </div>
     `;
-
-    list.appendChild(tr);
-  }
-
-  // UI : Delete Book From Table
-  static delBook(element) {
-    if(element.classList.contains('delete')) {
-        element.parentElement.parentElement.remove();
-    }
-  }
-
-  // UI : Showing Alert to user
-  static showAlert(message, className){
-    const div = document.createElement('div');
-    div.setAttribute('id', 'alert')
-    div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('.add-book');
-    const form = document.querySelector('#form-book-submit');
-    container.insertBefore(div, form);
-
-      // Vanish the div after some time
-    setTimeout(() => document.querySelector('#alert').remove(), 3000);
-  }
-
-  // UI : clearing the form input fields
-  static clearFields() {
-    document.querySelector('#book-title').value = '';
-    document.querySelector('#book-author').value = '';
-  }
+    const removeButton = document.createElement('div');
+    removeButton.classList.add('button-remove');
+    removeButton.textContent = 'Remove';
+    removeButton.onclick = () => {
+      books.removeBook(book);
+      loadBooksList();
+    };
+    bookDiv.appendChild(removeButton);
+    displaySection.appendChild(bookDiv);
+    displaySection.appendChild(document.createElement('hr'));
+  });
 }
 
-// Storage: Handle Local Storage
-class Store {
-  // Get books from local storage
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('book-list') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('book-list'));
-    }
-
-    return books;
-  }
-
-  // Add books to local storage
-  static addBook(book) {
-    const books = Store.getBooks();
-    books.push(book);
-    localStorage.setItem('book-list', JSON.stringify(books));
-  }
-
-  // Remove books from local storage
-  static rmBook(name) {
-    const books = Store.getBooks();
-    books.forEach((book, index) => {
-      if (book.name === name) {
-        books.splice(index, 1);
-      }
-    });
-    localStorage.setItem('book-list', JSON.stringify(books));
-  }
-}
-
-/*
-      Add Events to the Page 
-*/
-
-// Event : Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
-
-// Event : Add a Book
-document.querySelector('#form-book-submit').addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevent default submission
-
-  // Get Book Values
-  const name = document.querySelector('#book-title').value;
-  const author = document.querySelector('#book-author').value;
-  
-  // Add Simple Validation && Create the Book && Add to UI
-  if (name === '' || author === '') {
-    UI.showAlert('PLEASE FILL IN ALL FIELDS', 'danger');
-  }else {
-    const book = new Book(name, author);
-
-    UI.addBookToList(book);
-    Store.addBook(book);
-    
-    UI.showAlert('Book Added', 'success');
-    UI.clearFields();
+window.onload = () => {
+  // initialise booklist for the first time  with null array
+  if (localStorage.getItem('book-list') === null) {
+    let books = [];
+    localStorage.setItem('book-list',JSON.stringify(books));
   } 
-})
+  loadBooksList();
+};
 
-// Event : Remove a Book 
-document.querySelector('#display-book').addEventListener('click', (e) => {
-  UI.delBook(e.target);
-  Store.rmBook(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
-  UI.showAlert('Book Removed', 'success');
-})
+const bookForm = document.getElementById('form-book-submit');
+const bookTitle = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
+bookForm.onsubmit = (event) => {
+  event.preventDefault();
+  let books = new bookCollection();
+  books.addBook(new book(bookTitle.value,bookAuthor.value));
+  loadBooksList();
+  bookForm.reset();
+};
